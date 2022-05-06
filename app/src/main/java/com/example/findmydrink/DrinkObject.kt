c50481fccd00d21fa134c31d60a91eb92ab1b8cd
David <pdstu25@gmail.com>
2022-05-06 03:22:16 -0500

    added gui elements to drink activity, added more object items to DrinkObject.kt

--- app/src/main/java/com/example/findmydrink/DrinkObject.kt
+++ app/src/main/java/com/example/findmydrink/DrinkObject.kt
@@ -3,7 +3,9 @@ package com.example.findmydrink
 import java.io.Serializable
 
 class DrinkObject (
+    var idDrink : String = "",
     var strDrink : String = "",
-    var idDrink : String = ""
+    var strInstructions : String = "",
+    var strDrinkThumb : String = ""
         ) : Serializable {
 }
